d53f396851eddcfddbde7b6bc76b41aee87fccde
Timelinz <yunabdul1994@gmail.com>
2022-09-09 13:52:27 -0700

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** 
+ * main - Entry point
+ *
+ * Retue: Array 0 (success)
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a' ; low <= 'z'; low ++)
+		putchar(low);
+		putchar('\n');
+		return (0);
+}
