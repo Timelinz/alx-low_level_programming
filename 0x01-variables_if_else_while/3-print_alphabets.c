09ed044b63896b1b5334fec300176361944cd69f
Timelinz <yunabdul1994@gmail.com>
2022-09-10 13:32:13 -0700

    push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+char low;
+char upper;
+	for (low = 'a'; low <= 'z' ; low++)
+		putchar(low);
+	for (upper = 'A'; upper <= 'Z'; upper++)
+		putchar(upper);
+		putchar('\n');
+		return (0);
+}
