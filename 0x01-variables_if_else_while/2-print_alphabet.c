cd23aed7495a0155c4085cb6874dbcab6a2b6d46
Timelinz <yunabdul1994@gmail.com>
2022-09-11 01:43:03 -0700

    user

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,9 +11,8 @@
 int main(void)
 {
 	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+		for (low = 'a'; low <= 'z'; low++)
+			putchar(low);
+			putchar('\n');
 		return (0);
 }
