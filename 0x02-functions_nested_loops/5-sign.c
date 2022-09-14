e35a10b0edb668432a353659d1b7c101cac9e679
Timelinz <yunabdul1994@gmail.com>
2022-09-14 15:24:44 -0700

    uploading

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_sign - print the sign of a numeric value
+*@n: The value to test
+*
+*Return: 1 if n is positive.
+*-1 if n is negative.
+*Otherwise 0.
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
