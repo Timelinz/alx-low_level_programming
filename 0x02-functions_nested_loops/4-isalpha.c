5617fbac04902f9526f47b9899f636e36e99d769
Timelinz <yunabdul1994@gmail.com>
2022-09-14 15:01:45 -0700

    uploading

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isalpha - checks if a character is alphabetical
+* @c: The character to be checked
+*
+* Return: 1 if c is alphabetical.
+* Otherwise 0.
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
