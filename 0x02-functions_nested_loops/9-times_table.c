b3e055fdeead8ee84f5bf73a6f6e51f4bd365abf
Timelinz <yunabdul1994@gmail.com>
2022-09-14 15:55:58 -0700

    uploading

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - print a multiplication table of single-digit numbers
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 0; j < 10; ++j)
+		{
+			if (j == 0)
+				;
+			else if ((i * j) < 10)
+				_putchar(' ');
+			else
+				_putchar('0' + i * j / 10);
+
+			_putchar('0' + i * j % 10);
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
