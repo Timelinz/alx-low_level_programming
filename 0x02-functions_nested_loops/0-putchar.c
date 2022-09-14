b976134ed9a505f8dac715c6accd92a398df4c4f
Timelinz <yunabdul1994@gmail.com>
2022-09-13 20:12:31 -0700

    upload

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include <main.h>
 /**
 *main - Entry point
 *
 *Return: Always 0 (success)
 */
 
-int main(void)
-
+int main(void);
 {
-	int a;
-
-	while (a)
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
