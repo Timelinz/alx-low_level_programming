52d649a0789d3beca0845e58980967923b433b0f
Timelinz <yunabdul1994@gmail.com>
2022-09-14 15:31:05 -0700

    uploading

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_abs - Computes the absolute value of a number
+*@i: The value to test
+*
+*Return: The absolute value of i.
+*/
+int _abs(int i)
+{
+return (i < 0 ? -i : i);
+}
