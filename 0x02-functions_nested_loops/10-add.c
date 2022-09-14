a7e69e0de119eca831ee47b6edeee0c252cd9761
Timelinz <yunabdul1994@gmail.com>
2022-09-14 15:57:13 -0700

    uploading

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all integers from given value to 98 (inclusive)
+ * @n: The start of the range
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else
+	{
+		printf("%d, ", n);
+		print_to_98((n < 98) ? n + 1 : n - 1);
+	}
+}
