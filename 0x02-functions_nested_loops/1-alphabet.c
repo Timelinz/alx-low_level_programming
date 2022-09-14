67649091520603dfd4f002cdd26a592846ecb300
Timelinz <yunabdul1994@gmail.com>
2022-09-13 20:45:50 -0700

    upload

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints alphabets
+*
+*Description - Used
+*Return: Void
+*/
+
+void print_alphabet(void)
+{
+
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+}
