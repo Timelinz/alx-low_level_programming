3b38cdeb8084a1e0de94fd99051e79f0bf70994a
Timelinz <yunabdul1994@gmail.com>
2022-09-16 14:17:30 -0700

    upload

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,28 +1,43 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-int _isupper(int c);
-int _isdigit(int c);
-void print_numbers(void);
-void print_triangle(int size);
-void print_square(int size);
-void print_diagonal(int n);
-void print_line(int n);
-void more_numbers(void);
-void print_most_numbers(void);
-int mul(int a, int b);
-
-#endif
+fndef MAIN_H 
+ #define MAIN_H 
+  
+ #include <unistd.h> 
+  
+ int _putchar(char c); 
+   
+  /* TASK 0 */ 
+  int _isupper(int c); 
+    
+   /* TASK 1 */ 
+   int _isdigit(int c); 
+     
+    /* TASK 2 */ 
+    int mul(int a, int b); 
+      
+     /* TASK 3 */ 
+     void print_numbers(void); 
+       
+      /* TASK 4 */ 
+      void print_most_numbers(void); 
+        
+       /* TASK 5 */ 
+       void more_numbers(void); 
+         
+        /* TASK 6 */ 
+        void print_line(int n); 
+	  
+	 /* TASK 7 */ 
+	 void print_diagonal(int n); 
+	   
+	  /* TASK 8 */ 
+	  void print_square(int size); 
+	    
+	   /* TASK 10 */ 
+	   void print_triangle(int size); 
+	     
+	    /* TASK 11 */ 
+	     
+	    /* TASK 12 */ 
+	    void print_number(int n); 
+	      
+	     #endif /* MAIN_H */
