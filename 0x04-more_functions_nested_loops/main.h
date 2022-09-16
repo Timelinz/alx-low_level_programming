ed8490bd0f34d575dbd0596aa310e272c1e5b310
Timelinz <yunabdul1994@gmail.com>
2022-09-16 14:31:05 -0700

    upload

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,28 +16,30 @@ int mul(int a, int b);
 
 /* TASK 3 */
 void print_numbers(void);
-       
-      /* TASK 4 */ 
-      void print_most_numbers(void); 
-        
-       /* TASK 5 */ 
-       void more_numbers(void); 
-         
-        /* TASK 6 */ 
-        void print_line(int n); 
-	  
-	 /* TASK 7 */ 
-	 void print_diagonal(int n); 
-	   
-	  /* TASK 8 */ 
-	  void print_square(int size); 
-	    
-	   /* TASK 10 */ 
-	   void print_triangle(int size); 
-	     
-	    /* TASK 11 */ 
-	     
-	    /* TASK 12 */ 
-	    void print_number(int n); 
-	      
-	     #endif /* MAIN_H */
+
+/* TASK 4 */
+void print_most_numbers(void);
+
+/* TASK 5 */
+void more_numbers(void);
+
+/* TASK 6 */
+
+void print_line(int n);
+
+/* TASK 7 */
+void print_diagonal(int n);
+
+/* TASK 8 */
+void print_square(int size);
+
+/* TASK 10 */
+
+void print_triangle(int size);
+
+/* TASK 11 */
+
+/* TASK 12 */
+void print_number(int n);
+
+#endif /* MAIN_H */
