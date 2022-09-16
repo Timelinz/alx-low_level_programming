8dd701c00b710dc704a7bfabd4429bf3e0351bf3
Timelinz <yunabdul1994@gmail.com>
2022-09-16 14:00:53 -0700

    upload

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer (with no trailing newline)
+ * @n: integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n >= 0)
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		m = -1 * n;
+		if (m >= 10)
+			print_number(m / 10);
+		_putchar('0' + m % 10);
+	}
+
+}
