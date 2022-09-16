ddfce438443a904fa93f04117ea7712fd6bc1748
Timelinz <yunabdul1994@gmail.com>
2022-09-16 14:13:24 -0700

    upload

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -23,4 +24,5 @@ void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
 int mul(int a, int b);
+
 #endif
