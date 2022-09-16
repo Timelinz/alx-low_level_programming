1c016ceddea3b98dc8d55ca797bde51b1ae5ba74
Timelinz <yunabdul1994@gmail.com>
2022-09-16 14:55:49 -0700

    uploading

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,8 +2,6 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* positive_or_negative() - user
-* @i =user data
 * Return: 0
 */
 
@@ -11,7 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
