e37e6eea3e5ac90ce371e1bb071ad4708ed64c2d
Timelinz <yunabdul1994@gmail.com>
2022-09-19 14:42:27 -0700

    upload

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *reset_to_98 - :check the code to reset
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,6 @@
 
 int _putchar(char);
 
-void reset_to_98(int);
+void reset_to_98(int *n);
 
 #endif /* MAIN_H */
