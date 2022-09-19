cf889a87697d0ea95cb468eae28d2ca406115788
Timelinz <yunabdul1994@gmail.com>
2022-09-19 03:22:29 -0700

    upload

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*reset_to_98 - :check the code to reset
+*@n: char for usage
+*
+*/
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	n = 402;
+	_putchar(n);
+	reset_to_98(&n);
+	_putchar(n);
+}
