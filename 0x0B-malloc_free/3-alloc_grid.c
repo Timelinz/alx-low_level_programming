1399e3f2be379c3527c75b39a85ae00cdbd46d16
Timelinz <yunabdul1994@gmail.com>
2022-10-04 13:06:15 -0700

    moimoi malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create a 2-dimensional array with each element set to 0
+ * @width: desired number of columns
+ * @height: desired number of rows
+ *
+ * Return: NULL if memory allocation fails or any argument is less than 1,
+ * otherwise return a pointer to the first element of the array.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matrix, row, column;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	matrix = (int **) malloc(sizeof(int *) * height);
+
+	if (!matrix)
+		return (NULL);
+
+	for (row = 0; row < height; ++row)
+	{
+		matrix[row] = (int *) malloc(sizeof(int) * width);
+
+		if (!matrix[row])
+		{
+			while (--row > -1)
+				free(matrix[row]);
+			free(matrix);
+			return (NULL);
+		}
+
+		for (column = 0; column < width; ++column)
+			matrix[row][column] = 0;
+	}
+
+	return (matrix);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free a 2-dimensional array created by alloc_grid
+ * @grid: a pointer to the first column of the first row of the array
+ * @height: the number of rows in the array
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	while (height)
+		free(grid[--height]);
+	free(grid);
+}
